4518c279e94bc34d3c2e63c1048a14fbe2a29cb2
Adrian <52701496+futzu@users.noreply.github.com>
2022-11-22 20:20:54 +0000

    Added gob.Flag()

--- gob.go
+++ gob.go
@@ -21,8 +21,8 @@ func (g *Gob) Load(bites []byte) {
 	g.idx = 0
 }
 
-// chunk slices bitcount of bits and returns it as a uint64
-func (g *Gob) chunk(bitcount uint) *big.Int {
+// Chunk slices bitcount of bits and returns it as a uint64
+func (g *Gob) Chunk(bitcount uint) *big.Int {
 	j := new(big.Int)
 	d := g.idx + bitcount
 	j.SetString(g.bits[g.idx:d], 2)
@@ -30,45 +30,49 @@ func (g *Gob) chunk(bitcount uint) *big.Int {
 	return j
 }
 
-// UInt8 trims UInt64 to 8 bits
+// uint8 trims uint64 to 8 bits
 func (g *Gob) UInt8(bitcount uint) uint8 {
-	j := g.chunk(bitcount)
+	j := g.UInt64(bitcount)
 	return uint8(j)
 
 }
 
-// UInt16 trims UInt64 to 16 bits
+// uint16 trims uint64 to 16 bits
 func (g *Gob) UInt16(bitcount uint) uint16 {
-	j := g.chunk(bitcount)
+	j := g.UInt64(bitcount)
 	return uint16(j)
 
 }
 
-// UInt32 trims UInt64 to 32 bits
+// uint32 trims uint64 to 32 bits
 func (g *Gob) UInt32(bitcount uint) uint32 {
-	j := g.chunk(bitcount)
+	j := g.UInt64(bitcount)
 	return uint32(j)
 
 }
 
-// UInt64 is a wrapper for Chunk
+// uint64 is a wrapper for Chunk
 func (g *Gob) UInt64(bitcount uint) uint64 {
-	j := g.chunk(bitcount)
+	j := g.Chunk(bitcount)
 	return j.Uint64()
 
 }
 
-// Bool slices 1 bit and returns true for 1 , false for 0
-func (g *Gob) Bool() bool {
+// Flag slices 1 bit and returns true for 1 , false for 0
+func (g *Gob) Flag() bool {
 	var bitcount uint
 	bitcount = 1
 	j := g.UInt64(bitcount)
 	return j == 1
 }
 
+func (g *Gob) Bool() bool {
+	return g.Flag()
+}
+
 // Float slices bitcount of bits and returns  float64
 func (g *Gob) Float(bitcount uint) float64 {
-	j := g.chunk(bitcount)
+	j := g.UInt64(bitcount)
 	return float64(j)
 }
 
